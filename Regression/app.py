4c82ce0b9965a8aabfff67a8b9180d15931b544f
Sameerverma32 <sameerverma8318@gmail.com>
2025-07-10 15:21:45 +0530

    ml

---
+++ Regression/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import joblib 
+import numpy 
+
+#load the model
+with open('models\simple_linear_regression.pkl', 'rb') as file:
+    model = joblib.load(file)
+    
+    #title
+st.set_page_config(page_title='Salary Predictor')
+st.title('Salary Predictor App')
+st.subheader('Predict your salary based on your experience')
+st.markdown("Simply input your years of experience and get the predicted salary.")
+
+#side bar
+st.sidebar.header('Enter your details👇')
+experience = st.sidebar.slider("Years of Experience", 
+                               min_value=0.0, max_value=20.0, step=0.5)
+
+#Buttons to predict
+if st.sidebar.button("Predict Salary"):
+    #predict salary
+#salary = model.predict(np.array([[experience]]))[0]
+
+#Display result
+#st.success(f"Predicted Salary: Rs.{salary:,.2f}")
+
+ #Additional Info
+ st.info("This prediction is based on a simpple Linear Regression")  
+
+#footer 
+st.markdown("-------")
+st.markdown("Made with using Streamlit")
